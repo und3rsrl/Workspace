d21310d63674b4a0725e3c9bd4e9e111231ce389
Alex Chiurtu <alex.chiurtu@gmail.com>
2017-01-05 11:43:15 +0200

    Add files via upload

---
+++ Game Development/Snake/assets/js/main.js
@@ -0,0 +1,16 @@
+var game;
+
+// Create a new game instance 600px wide and 450px tall:
+game = new Phaser.Game(600, 450, Phaser.AUTO, '');
+
+// First parametre is how our state will be called.
+// Second parameter is an object containing the need methods for state functionality
+
+game.state.add('Menu', Menu);
+
+// Adding the Game state.
+game.state.add('Game', Game);
+
+game.state.add('Game_Over', Game_Over);
+game.state.start('Menu');
+
